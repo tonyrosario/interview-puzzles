1ec9d91446bb3a137afcff75e3f4dc77c3ed37b8
Tony Rosario <tony.rosario@gmail.com>
2011-07-12 22:06:56 -0500

    Formatting

--- src/test/java/coding/StringReverserTest.java
+++ src/test/java/coding/StringReverserTest.java
@@ -4,12 +4,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StringReverserTest {
-	
+
 	@Test
 	public void reverseReturnsAReversedString() {
 		String target = "Madam, I'm Adam";
 		String expected = "madA m'I ,madaM";
-		
+
 		StringReverser reverser = new StringReverser();
 		assertEquals(expected, reverser.reverse(target));
 	}
