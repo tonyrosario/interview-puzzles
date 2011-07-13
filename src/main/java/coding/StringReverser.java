20768762886e1a8841291f95352b352d6787ebb7
Tony Rosario <tony.rosario@gmail.com>
2011-07-12 22:06:41 -0500

    Added impl of reverse

--- src/main/java/coding/StringReverser.java
+++ src/main/java/coding/StringReverser.java
@@ -3,7 +3,14 @@ package coding;
 public class StringReverser {
 
 	public String reverse(String string) {
-		return "";
+		char[] charArray = string.toCharArray();
+		int length = charArray.length - 1;
+		for (int i = 0; i < length / 2; i++) {
+			char c = charArray[i];
+			charArray[i] = charArray[length - i];
+			charArray[length - i] = c;
+		}
+		return new String(charArray);
 	}
 
 }
