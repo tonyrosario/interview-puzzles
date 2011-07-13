a4bc2299b6b8aed4ebd2af08713f600536593802
Tony Rosario <tony.rosario@gmail.com>
2011-07-12 23:04:22 -0500

    Added warning about inefficient impl.

--- src/main/java/coding/FibonnaciCalculator.java
+++ src/main/java/coding/FibonnaciCalculator.java
@@ -2,6 +2,7 @@ package coding;
 
 public class FibonnaciCalculator {
 
+	//when number is around 43, the performance really starts to slow down.
 	public int calculate(int number) {
 		return number <= 1 ? number : calculate(number-1) + calculate(number - 2);
 	}
