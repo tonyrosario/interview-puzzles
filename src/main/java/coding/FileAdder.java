4862176b1749e219373fc7a759339c634f7ae2ca
Tony Rosario <tony.rosario@gmail.com>
2011-07-15 00:40:01 -0500

    Added framework for reading the file.

--- src/main/java/coding/FileAdder.java
+++ src/main/java/coding/FileAdder.java
@@ -1,8 +1,18 @@
 package coding;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class FileAdder {
 
 	public int calculateSum(String file) {
-		return 33;
+		int total = 0;
+		try {
+			BufferedReader inFile = new BufferedReader(new FileReader(file));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return total;
 	}
 }
