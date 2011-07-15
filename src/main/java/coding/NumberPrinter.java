4bdd79cd68a1619ee8ad6716451cc2c6f8bab9e3
Tony Rosario <tony.rosario@gmail.com>
2011-07-15 00:49:14 -0500

    New class and stub method.

---
+++ src/main/java/coding/NumberPrinter.java
@@ -0,0 +1,13 @@
+package coding;
+
+public class NumberPrinter {
+
+	public void printOddNumbers(int minNumber, int maxNumber) {
+		
+	}
+	
+	public static void main(String[] args) {
+		NumberPrinter numberPrinter = new NumberPrinter();
+		numberPrinter.printOddNumbers(1, 99);
+	}
+}
