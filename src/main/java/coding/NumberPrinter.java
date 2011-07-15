d1968a9d161702d6ac9240f3a26e31830b8f2290
Tony Rosario <tony.rosario@gmail.com>
2011-07-15 00:51:17 -0500

    brute force impl... but what if minNumber was even?

--- src/main/java/coding/NumberPrinter.java
+++ src/main/java/coding/NumberPrinter.java
@@ -3,9 +3,11 @@ package coding;
 public class NumberPrinter {
 
 	public void printOddNumbers(int minNumber, int maxNumber) {
-		
+		for (int i = minNumber; i <= maxNumber; i += 2) {
+			System.out.println(String.format("%2d", i));
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		NumberPrinter numberPrinter = new NumberPrinter();
 		numberPrinter.printOddNumbers(1, 99);
