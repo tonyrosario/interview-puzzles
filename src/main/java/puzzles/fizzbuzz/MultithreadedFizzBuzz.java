4f3cf32d15bc05278d44b6bca2355999e69869e4
Tony Rosario <tony.rosario@gmail.com>
2016-01-13 18:12:57 -0600

    added class for multithreaded FizzBuzz

---
+++ src/main/java/puzzles/fizzbuzz/MultithreadedFizzBuzz.java
@@ -0,0 +1,10 @@
+package puzzles.fizzbuzz;
+
+/**
+ * Solve FizzBuzz in a multithreaded way. Taken from Cracking the Code Interview.
+ */
+public class MultithreadedFizzBuzz {
+    public String solve(final int number) {
+        throw new UnsupportedOperationException();
+    }
+}
