bd316802dbc5da06af62e88494af308241998ef4
Tony Rosario <tony.rosario@gmail.com>
2016-01-13 18:07:26 -0600

    added fizzbuzz

---
+++ src/main/java/puzzles/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package puzzles.fizzbuzz;
+
+public class FizzBuzz {
+    public String solve(final int number) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/puzzles/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,44 @@
+package puzzles.fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void threeReturnsFizz() {
+        Assert.assertEquals(fizzBuzz.solve(3), "Fizz");
+    }
+
+    @Test
+    public void fiveReturnsBuzz() {
+        Assert.assertEquals(fizzBuzz.solve(5), "Buzz");
+    }
+
+    @Test
+    public void fifteenReturnsFizzBuzz() {
+        Assert.assertEquals(fizzBuzz.solve(15), "FizzBuzz");
+    }
+
+    @Test
+    public void oneReturnsNull() {
+        Assert.assertNull(fizzBuzz.solve(1));
+    }
+
+    @Test
+    public void zeroReturnsFizzBuzz() {
+        Assert.assertNull(fizzBuzz.solve(0));
+    }
+
+    @Test
+    public void twoReturnsNull() {
+        Assert.assertNull(fizzBuzz.solve(2));
+    }
+}
